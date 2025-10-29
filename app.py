5814aeead9307964404342fa944ab8d3a15fe176
FMO-OMF <mkvv121@naver.com>
2025-10-29 19:20:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
-st.title("my first app")
+st.title("my first app2025")
 st.write("Hello")
 name = st.text_input("name?")
 if st.button("HELLO"):
     st.success(f"안녕하세요{name}님")
+
 
